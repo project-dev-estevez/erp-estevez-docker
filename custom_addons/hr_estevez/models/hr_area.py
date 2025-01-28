ac154a8b524b607673996f1b23e4f979d8b0437a
Irvin Fernandez <ifernandez.estevezjor@gmail.com>
2025-01-28 14:06:12 -0600

    EVZ-434-Validación en campo Jefe Inmediato

--- custom_addons/hr_estevez/models/hr_area.py
+++ custom_addons/hr_estevez/models/hr_area.py
@@ -11,6 +11,7 @@ class HrArea(models.Model):
     coordinator_id = fields.Many2one('hr.employee', string='Coordinador')
     direction_id = fields.Many2one('hr.direction', string='Dirección')
     job_id = fields.Many2one('res.job', string='Puesto')
+    area_ids = fields.One2many('hr.area', 'department_id', string='Areas')
     total_employees = fields.Integer(string='Total Empleados', compute='_compute_total_employees')
 
     @api.onchange('department_id')
