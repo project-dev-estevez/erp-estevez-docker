623b0f8e2b600e493946bd609d32d5b7100df64f
Alejandro Gómez <alejandrogomez2339@gmail.com>
2025-02-19 10:22:58 -0500

    Employees inf.personal

--- custom_addons/hr_estevez/models/hr_employee.py
+++ custom_addons/hr_estevez/models/hr_employee.py
@@ -57,6 +57,16 @@ class HrEmployee(models.Model):
     country_id = fields.Many2one('res.country', string='País', default=lambda self: self.env.ref('base.mx').id)
     is_mexico = fields.Boolean(string="Is Mexico", compute="_compute_is_mexico", store=False)
 
+    marital = fields.Selection([
+        ('single', 'Soltero(a)'),
+        ('married', 'Casado(a)'),
+        ('cohabitant', 'Cohabitante Legal'),
+        ('widower', 'Viudo(a)'),
+        ('divorced', 'Divorciado(a)')
+    ], string='Estado Civil', required=True, tracking=True)
+
+    spouse_birthdate = fields.Date(string="Spouse Birthdate", groups="hr.group_hr_user", store=False)
+
     @api.depends('country_id')
     def _compute_is_mexico(self):
         for record in self:
