e52f414c526991104ccf5d6ade1891fa40e62ec9
Diego Sánchez Dueñas <diego.sanchez@estevezjor.mx>
2025-02-20 18:11:47 -0600

    Dynamic form - EVZ-479

--- custom_addons/purchase_estevez/models/purchase_requisition.py
+++ custom_addons/purchase_estevez/models/purchase_requisition.py
@@ -25,7 +25,7 @@ class PurchaseRequisition(models.Model):
 
     state_id = fields.Many2one(
         'res.country.state',
-        string='Mercado',
+        string='Estado',
         domain="[('country_id.code', '=', 'MX')]"
     )
 
@@ -36,7 +36,7 @@ class PurchaseRequisition(models.Model):
         ('camp', 'Campamento'),
         ('lodging', 'Hospedaje'),
         ('store', 'Almacén'),
-        ('machinery_equipment', 'Maquinaría y equpo'),
+        ('machinery_equipment', 'Maquinaría y equipo'),
         ('service_payment', 'Pago de servicio'),
         ('freight', 'Flete'),
     ], string='Tipo de Solicitud')
@@ -75,6 +75,12 @@ class PurchaseRequisition(models.Model):
     )
 
     #Campos de campamento
+    dynamic_page_title = fields.Char(
+        string="Título de la página",
+        compute="_compute_dynamic_page_title",
+        store=True
+    )
+
     vehicle_count = fields.Integer(
         string="Número de vehículos",
         default=1,  # Valor predeterminado
@@ -109,15 +115,18 @@ class PurchaseRequisition(models.Model):
     )
 
     service_ids = fields.Many2many('purchase.requisition.service', string="Servicios")
-    employee_id = fields.Many2one('hr.employee', string="Persona responsable")
-
-    responsible_number = fields.Integer(
-        string="Número del responsable",
+    area = fields.Integer(
+        string="Área mínima requerida en metros cuadrados",
         default=1,  # Valor predeterminado
-        help="Numero requerido",
+        help="Cantidad total de metros cuadrados requeridos",
         # Restricciones opcionales:
         required=True,  # Obligatorio
         positive=True,  # Solo números positivos
+    )
+    employee_id = fields.Many2one('hr.employee', string="Persona responsable")
+
+    responsible_number = fields.Char(
+        string="Número del responsable",
     )
 
     employee_ids = fields.Many2many('hr.employee', string="Listado de personal")
@@ -127,6 +136,125 @@ class PurchaseRequisition(models.Model):
 
     letter_responsibility = fields.Binary(string="Subir archivo")
     letter_responsibility_name = fields.Char(string="Nombre del archivo")
+
+    #campos maquinaría
+    machinery_equipment_required = fields.Selection([
+        ('hoist', 'Montacargas'),
+        ('backhoe', 'Retroescabadora'),
+        ('trencher', 'Zanjadora'),
+        ('emergency_plant', 'Planta de emergencia'),
+        ('service_payment', 'Pago de servicio'),
+        ('luminaries', 'Luminarias'),
+        ('other', 'Otros'),
+    ], string='Maquinaría o equipo que requiere')
+
+    other_machinery_equipment = fields.Char(string='Otros')
+
+    capacity_hoist = fields.Integer(
+        string="Capacidad de montacargas",
+        default=1,  # Valor predeterminado
+        help="Capacidad requerida",
+        # Restricciones opcionales:
+        required=True,  # Obligatorio
+        positive=True,  # Solo números positivos
+    )
+
+    stowage_height = fields.Integer(
+        string="Altura de estiba requerida",
+        default=1,  # Valor predeterminado
+        help="Altura requerida",
+        # Restricciones opcionales:
+        required=True,  # Obligatorio
+        positive=True,  # Solo números positivos
+    )
+
+    fuel_type = fields.Selection([
+        ('gas', 'Gas'),
+        ('diesel', 'Diésel'),
+        ('gasoline', 'Gasolina'),
+    ], string='Tipo de combustión')
+
+    operator_required = fields.Boolean(string="¿Requiere operador?", default=False)
+    terrain_tires = fields.Boolean(string="¿Llantas todo terreno?", default=False)
+
+    street_address = fields.Char(string='Calle')
+    city_id = fields.Many2one(
+        'res.city',  # Modelo personalizado
+        string='Ciudad/Municipio',
+        domain="[('state_id', '=', state_id)]"  # Filtra ciudades por estado
+    )
+    zip_code = fields.Char(string='Código postal')
+    arrival_time = fields.Datetime(
+        string="Hora de llegada",
+        help="Selecciona la hora estimada de llegada"
+    )
+
+    # Campos de pago de servicio
+    service_payment = fields.Selection([
+        ('phone', 'Teléfono'),
+        ('internet', 'Internet'),
+        ('water', 'Agua'),
+        ('gas', 'Gas'),
+        ('light', 'Luz'),
+        ('rent', 'Renta'),
+    ], string='Servicio a pagar')
+    amount_to_pay = fields.Integer(
+        string="Monto a pagar",
+        default=1,  # Valor predeterminado
+        help="Monto requerido",
+        # Restricciones opcionales:
+        required=True,  # Obligatorio
+        positive=True,  # Solo números positivos
+    )
+    period_to_pay = fields.Date(string="Periodo a pagar")
+    
+    payment_receipt = fields.Binary(string="Subir archivo")
+    payment_receipt_name = fields.Char(string="Nombre del archivo")
+
+    deposit_person = fields.Boolean(string="¿Deposito a una persona para pago de servicio?", default=False)
+
+    additional_specifications = fields.Binary(string="Subir archivo")
+    additional_specifications_name = fields.Char(string="Nombre del archivo")
+
+    #campos de flete
+    description_merchandise = fields.Char(string="Descripción de la mercancía")
+    weight = fields.Integer(
+        string="Peso",
+        default=1,  # Valor predeterminado
+        help="Peso requerido",
+        # Restricciones opcionales:
+        required=True,  # Obligatorio
+        positive=True,  # Solo números positivos
+    )
+    dimension = fields.Char(string="Dimensiones")
+    date_use = fields.Date(string="Fecha de uso")
+    product = fields.Selection([
+        ('parquet', 'Entarimado'),
+        ('simple_packaging', 'Embalaje Sencillo'),
+    ], string='Producto')
+
+    street_number_origin = fields.Char(string="Calle y número")
+    colony_origin = fields.Char(string="Colonia")
+    city_origin = fields.Char(string="Ciudad")
+    zip_code_origin = fields.Char(string="Código Postal")
+
+    street_number_destination = fields.Char(string="Calle y número")
+    colony_destination = fields.Char(string="Colonia")
+    city_destination = fields.Char(string="Ciudad")
+    zip_code_destination = fields.Char(string="Código Postal")
+
+
+    delivery_name = fields.Char(string="Nombre de quien entrega")
+    delivery_phone = fields.Char(
+        string="Teléfono de quien entregar",
+    )
+    receiver_name = fields.Char(string="Nombre de quine recibe")
+    receiver_phone = fields.Char(
+        string="Teléfono de quien recibe",
+    )
+
+
+    
 
 # Acciones de estado
     def action_approve(self):
@@ -251,3 +379,22 @@ class PurchaseRequisition(models.Model):
                 record.duration_days = f"{days} días y {nights} noches"
             else:
                 record.duration_days = "0 días y 0 noches"
+
+    @api.depends('request_type')
+    def _compute_dynamic_page_title(self):
+        for rec in self:
+            if rec.request_type == 'camp':
+                rec.dynamic_page_title = "Información del Campamento"
+            elif rec.request_type == 'lodging':
+                rec.dynamic_page_title = "Información del Hospedaje"
+            elif rec.request_type == 'store':
+                rec.dynamic_page_title = "Información del Almacén"
+            else:
+                rec.dynamic_page_title = ""
+    
+    def action_save(self):
+        # Aquí puedes agregar cualquier lógica adicional antes de guardar
+        self.ensure_one()
+        self.write({'state': self.state})  # Esto guarda el registro
+        _logger.info("Requisición guardada con éxito")
+        return True
