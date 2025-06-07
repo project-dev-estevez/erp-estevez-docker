0344a5317793b398812ddfefdce3a03d04882108
Alejandro Gómez <alejandrogomez2339@gmail.com>
2025-06-07 18:30:15 -0500

    test script pipeline

--- config/deploy-script.sh
+++ config/deploy-script.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 set -ex
 
-LOGFILE="/tmp/deploy-erp.log"
+echo "Reiniciando el servicio odoo18..."
+sudo systemctl restart odoo18
 
-echo "INICIO DEL SCRIPT" | tee "$LOGFILE"
-echo "Directorio actual:" | tee -a "$LOGFILE"
-pwd | tee -a "$LOGFILE"
-
-echo "📁 Listando contenido del directorio actual:" | tee -a "$LOGFILE"
-ls -la | tee -a "$LOGFILE"
-
-echo "📁 Listando contenido del home de odoo18:" | tee -a "$LOGFILE"
-sudo su - odoo18 -c 'ls -la $HOME' | tee -a "$LOGFILE"
-
-echo "==== CONTENIDO DEL LOG ===="
-cat "$LOGFILE"
+echo "Servicio odoo18 reiniciado correctamente."
