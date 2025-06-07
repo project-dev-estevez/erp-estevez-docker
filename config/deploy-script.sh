9d3c6b6ebe07bc494db0ab2588ca0697d93f77b4
Alejandro Gómez <alejandrogomez2339@gmail.com>
2025-06-07 17:55:13 -0500

    test script pipeline

--- config/deploy-script.sh
+++ config/deploy-script.sh
@@ -1,3 +1,12 @@
-echo -n 1
-echo -n 2
-echo 3
+#!/bin/bash
+set -e
+
+echo "Cambiando a usuario odoo18 y listando el home:"
+sudo su - odoo18 -c "ls -1 ~" > /tmp/odoo18_home.txt
+echo "Carpetas en el home de odoo18:"
+cat /tmp/odoo18_home.txt
+
+echo "Entrando a la carpeta odoo-erp y listando su contenido:"
+sudo su - odoo18 -c "cd ~/odoo-erp && ls -1" > /tmp/odoo18_odooerp.txt
+echo "Contenido de ~/odoo-erp:"
+cat /tmp/odoo18_odooerp.txt
