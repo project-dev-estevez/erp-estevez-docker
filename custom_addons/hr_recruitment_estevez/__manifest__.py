00777ce4691f268de7d5e21b4847748aee64a173
Ruth Amaranta <ruth.rivera@estevezjor.mx>
2025-06-17 12:21:26 -0600

    Vista

--- custom_addons/hr_recruitment_estevez/__manifest__.py
+++ custom_addons/hr_recruitment_estevez/__manifest__.py
@@ -31,7 +31,7 @@
         # Archivos de vistas
         'views/hr_applicant_document_views.xml',
         'views/hr_applicant_view_form_inherit.xml',
-        'views/hr_candidate_view_form_inherit.xml',
+        #'views/hr_candidate_view_form_inherit.xml',
         "views/hr_recruitment_menu_views.xml",
         "views/hr_requisition_views.xml",
         "views/hr_applicant_view_list_general_report_inherit.xml",
