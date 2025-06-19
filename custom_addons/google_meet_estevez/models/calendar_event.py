4514ecdbc2d240e3c63d0188cce29533a8494d20
Diego Sánchez Dueñas <diego.sanchez@estevezjor.mx>
2025-06-19 10:34:32 -0600

    Create field

--- custom_addons/google_meet_estevez/models/calendar_event.py
+++ custom_addons/google_meet_estevez/models/calendar_event.py
@@ -1,26 +1,25 @@
-from odoo import models, api, fields
+from odoo import models, fields, api
 import uuid
 
 class CalendarEvent(models.Model):
     _inherit = 'calendar.event'
     
-    # Campo adicional para controlar el tipo de meet
-    is_google_meet = fields.Boolean(string="Usar Google Meet", default=True)
     
-    # Sobrescribir creación para evitar meet de Odoo
+    is_google_meet = fields.Boolean(
+        string="Usar Google Meet",
+        default=True,
+        help="Genera un enlace de Google Meet al sincronizar"
+    )
+    
     @api.model_create_multi
     def create(self, vals_list):
-        # Desactivar videollamada automática de Odoo
         for vals in vals_list:
             if 'videocall_location' not in vals:
                 vals['videocall_location'] = False
         return super().create(vals_list)
     
-    # Método modificado para Google Meet
     def _google_values(self):
         values = super()._google_values()
-        
-        # Solo si está marcado para Google Meet
         if self.is_google_meet and not self.videocall_location:
             values['conferenceData'] = {
                 'createRequest': {
