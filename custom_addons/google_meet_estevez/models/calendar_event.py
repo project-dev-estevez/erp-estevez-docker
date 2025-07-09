528e17390514b074871054ddc23e3b3e4ea87a74
Diego Sánchez Dueñas <diego.sanchez@estevezjor.mx>
2025-07-09 17:24:45 -0600

    Funciones meet

--- custom_addons/google_meet_estevez/models/calendar_event.py
+++ custom_addons/google_meet_estevez/models/calendar_event.py
@@ -3,14 +3,53 @@ from odoo.exceptions import UserError
 import uuid
 import logging
 from odoo.addons.google_calendar.utils.google_calendar import GoogleCalendarService
-from odoo.addons.google_calendar.models.google_sync import GoogleSync
 
 _logger = logging.getLogger(__name__)
 
 class CalendarEvent(models.Model):
     _inherit = 'calendar.event'
 
-    # ... (otros campos y métodos)
+    is_google_meet = fields.Boolean(
+        string="Usar Google Meet",
+        default=True,
+    )
+    show_meet_button = fields.Boolean(
+        string="Mostrar botón Meet",
+        compute="_compute_show_meet_button",
+        store=True,
+    )
+
+    @api.depends('is_google_meet', 'videocall_location', 'google_id')
+    def _compute_show_meet_button(self):
+        for event in self:
+            # Condición simplificada para mejor rendimiento
+            event.show_meet_button = (
+                event.is_google_meet and 
+                not event.videocall_location and 
+                bool(event.google_id)
+            )
+
+    def _create_google_meet(self):
+        """Crea una reunión de Google Meet"""
+        service = GoogleCalendarService(self.env['google.service'])
+        try:
+            res = service.patch(
+                calendar_id='primary',
+                event_id=self.google_id,
+                body={
+                    'conferenceData': {
+                        'createRequest': {
+                            'conferenceSolutionKey': {'type': 'hangoutsMeet'},
+                            'requestId': str(uuid.uuid4()),
+                        }
+                    }
+                },
+                params={'conferenceDataVersion': 1},
+            )
+            return res.get('hangoutLink')
+        except Exception as e:
+            _logger.error("Error creando Google Meet: %s", e)
+            return False
 
     def action_force_create_meet(self):
         self.ensure_one()
