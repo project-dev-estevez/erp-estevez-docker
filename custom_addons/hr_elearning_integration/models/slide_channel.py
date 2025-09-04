6c5be2fd3f4fb85079413e91b6e8dfe3413ba54f
Alejandro Gómez <alejandrogomez2339@gmail.com>
2025-09-04 07:55:17 -0500

    Wizard before generate certificate

--- custom_addons/hr_elearning_integration/models/slide_channel.py
+++ custom_addons/hr_elearning_integration/models/slide_channel.py
@@ -4,4 +4,15 @@ class SlideChannel(models.Model):
     _inherit = 'slide.channel'
 
     def generate_course_certificate(self):
-        return self.env.ref('hr_elearning_integration.action_report_course_certificate').report_action(self)
+        return {
+            'type': 'ir.actions.act_window',
+            'name': 'Generar Certificado',
+            'res_model': 'certificate.wizard',
+            'view_mode': 'form',
+            'view_id': self.env.ref('hr_elearning_integration.view_certificate_wizard_form').id,
+            'target': 'new',
+            'context': {
+                'active_id': self.id,
+                'default_course_id': self.id,
+            },
+        }
