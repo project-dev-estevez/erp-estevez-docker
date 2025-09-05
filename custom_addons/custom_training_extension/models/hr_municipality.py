7f5ad700c27c56897c36a9c71f6bb0238a93d231
Ruth Amaranta <ruth.rivera@estevezjor.mx>
2025-09-05 08:44:23 -0600

    Carga masiva

---
+++ custom_addons/custom_training_extension/models/hr_municipality.py
@@ -0,0 +1,9 @@
+from odoo import models, fields
+
+class HrMunicipality(models.Model):
+    _name = 'hr.municipality'
+    _description = 'Catálogo de Municipios'
+
+    code = fields.Char(string="Clave Municipio", required=True)
+    name = fields.Char(string="Nombre Municipio", required=True)
+    state_id = fields.Many2one('hr.state', string="Estado", required=True)
