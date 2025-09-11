b1eb10264597567f644f5a40cb77b26cfe9e76ac
Diego Sánchez Dueñas <diego.sanchez@estevezjor.mx>
2025-09-11 17:29:21 -0600

    data

--- custom_addons/hr_incapacity/__manifest__.py
+++ custom_addons/hr_incapacity/__manifest__.py
@@ -9,7 +9,6 @@
     'author': 'Estevez.Jor',
     'depends': ['hr', 'hr_holidays'],
     'data': [
-        'data/hr_incapacity_data.xml',
         'security/hr_incapacity_security.xml',
         'security/ir.model.access.csv',
         'views/hr_incapacity_views.xml',
