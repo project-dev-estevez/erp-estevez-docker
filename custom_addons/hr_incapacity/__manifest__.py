a3bfc6baec033fee6563902ec10585c118dd4c02
Diego Sánchez Dueñas <diego.sanchez@estevezjor.mx>
2025-09-11 17:35:20 -0600

    Solo instalar modulo

--- custom_addons/hr_incapacity/__manifest__.py
+++ custom_addons/hr_incapacity/__manifest__.py
@@ -9,7 +9,6 @@
     'author': 'Estevez.Jor',
     'depends': ['hr', 'hr_holidays'],
     'data': [
-        'views/hr_incapacity_views.xml',
         'views/hr_employee_views.xml',
     ],
     'demo': [],
