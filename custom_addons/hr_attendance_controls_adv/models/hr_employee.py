1f9cab993a2c422e20e44643a0424962266f1a8d
Alejandro Gómez <alejandrogomez2339@gmail.com>
2025-10-08 14:24:52 -0500

    attendance status

--- custom_addons/hr_attendance_controls_adv/models/hr_employee.py
+++ custom_addons/hr_attendance_controls_adv/models/hr_employee.py
@@ -5,6 +5,28 @@ class HrEmployee(models.Model):
     
     user_faces = fields.One2many("hr.employee.faces", "employee_id", "Caras")
 
+    attendance_status = fields.Selection([
+        ('pre_work', 'Pre-work'),
+        ('in_work', 'In work'),
+        ('post_work', 'Post-work'),
+    ], string="Estado de asistencia", compute='_compute_attendance_status', store=False)
+
+    @api.depends('attendance_ids.check_in', 'attendance_ids.check_out')
+    def _compute_attendance_status(self):
+        today = fields.Date.today()
+        for employee in self:
+            attendances_today = employee.attendance_ids.filtered(
+                lambda a: a.check_in and a.check_in.date() == today
+            )
+            if not attendances_today:
+                employee.attendance_status = 'pre_work'
+            else:
+                attendance = attendances_today.sorted('check_in', reverse=True)[0]
+                if attendance.check_out:
+                    employee.attendance_status = 'post_work'
+                else:
+                    employee.attendance_status = 'in_work'
+
 class HrEmployeeFaces(models.Model):
     _name = "hr.employee.faces"
     _description = "Face Recognition Images"
